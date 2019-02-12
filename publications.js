563b691b622b6201d98c0bfb6d1f73964fada59a
Or Litany <orlitany@fb.com>
2019-02-12 06:38:13 -0800

    automatic publication records
    publications are now in a separate js file and are read automatically.

---
+++ publications.js
@@ -0,0 +1,42 @@
+data = {
+    "publications": [
+        {
+            "name": "Deformable Shape Completion with Graph Convolutional Autoencoders",
+            "authors": "<strong>Or Litany</strong>, Alex M. Bronstein, Michael M. Bronstein, Ameesh Makadia",
+            "publication": "Computer Vision and Pattern Recognition (CVPR), 2018",
+            "image": "./OL_files/shapeComp.png",
+            "paper": "https://arxiv.org/pdf/1712.00268v1.pdf",
+            "bibtex": `@article{litany2017deformable,
+Author    = {Or Litany and Alex Bronstein and Michael Bronstein and Ameesh Makadia},
+Title     = {Deformable Shape Completion with Graph Convolutional Autoencoders},
+Journal   = {CVPR},
+Year      = {2018}}`
+        },
+        {
+            "name": "Efficient Deformable Shape Correspondence via Kernel Matching",
+            "authors": "Matthias Vestner, Zorah Lähner, Amit Boyarski, <strong>Or Litany</strong>, et. al.",
+            "publication": "Intl. Conference on 3D Vision (3DV), 2017",
+            "image": "./OL_files/vestner-3dv17.png",
+            "paper": "https://arxiv.org/pdf/1707.08991.pdf",
+            "code": "https://github.com/zorah/KernelMatching",
+            "bibtex": `@article{vestner2017efficient,
+Author    = {Matthias Vestner and Zorah Lähner and Amit Boyarski and Or Litany and others},
+Title     = {Efficient Deformable Shape Correspondence via Kernel Matching},
+Journal   = {3DV},
+Year      = {2017}}`
+        },
+        {
+            "name": "Deep Functional Maps: Structured Prediction for Dense Shape Correspondence",
+            "authors": "<strong>Or Litany</strong>, Tal Remez, Emanuele Rodol{\`a}, Alex M. Bronstein, Michael M. Bronstein",
+            "publication": "International Conference on Computer Vision (ICCV), 2017",
+            "image": "./OL_files/FMNet.png",
+            "paper": "http://vision.in.tum.de/_media/spezial/bib/litany-iccv17.pdf",
+            "code": "https://github.com/orlitany/DeepFunctionalMaps",
+            "bibtex": `@article{litany2017fmnet,
+Author    = {Or Litany and Tal Remez and Emanuele Rodol{\`a} and Alex Bronstein and Michael Bronstein},
+Title     = {Deep Functional Maps: Structured Prediction for Dense Shape Correspondence},
+Journal   = {ICCV},
+Year      = {2017}}`
+        },
+    ]
+};
